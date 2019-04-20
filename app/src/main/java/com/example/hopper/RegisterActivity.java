0df09958fb5e4a0d9d13dec20934234842353652
brendendriver <46637064+brendendriver@users.noreply.github.com>
2019-04-20 12:33:23 -0400

    Update RegisterActivity.java

--- app/src/main/java/com/example/hopper/RegisterActivity.java
+++ app/src/main/java/com/example/hopper/RegisterActivity.java
@@ -1,7 +1,11 @@
 package com.example.hopper;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -28,8 +32,21 @@ public class RegisterActivity extends AppCompatActivity {
         btnSignup = findViewById(R.id.btnSignup);
         btnFBSignup = findViewById(R.id.btnFBSignup);
         tvExistLogin = findViewById(R.id.tvExistLogin);
+        tvExistLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                goLoginActivity();
+            }
+        });
 
     }
 
+    private void goLoginActivity() {
+        Log.d(TAG, "Navigating to Login Activity");
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
 
 }
